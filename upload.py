673afdffb2a360df2059f5241036a2cb29eea4dd
drewpager <drewpage@shredlights.com>
2022-11-16 07:24:59 -0800

    CSV writerow properly and remove file extension for raw name

--- upload.py
+++ upload.py
@@ -12,6 +12,7 @@ import os
 from os import listdir
 from os.path import isfile, join
 import csv
+from pathlib import Path
 # Import to format the JSON responses
 # ==============================
 import json
@@ -25,28 +26,24 @@ config = cloudinary.config()
 
 print("****1. Set up and configure the SDK:****\nCredentials: ", config.cloud_name, config.api_key, "\n")
 
-# Get File system
+# Get File system // UPDATE USING PWD in TERMINAL
 # ==============================
 mypath = "/Users/drewpage/Desktop/Personal/Video"
 files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-print(files)
+
 for file in files:
-  print(mypath + "/" + file)
+  route = mypath + "/" + file
+  print(route)
+  
+  # Upload to Cloudinary: Change folder when ready! 
   cloudinary.uploader.upload(mypath + "/" + file, folder="TH_assets", resource_type="video")
 
   srcURL = cloudinary.CloudinaryImage(file).build_url()
-
-  with open('videos.csv', 'wb') as csvfile:
-    filewriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    filewriter.writerow(["title", "cloudinary_url"])
-    filewriter.writerow({ 'title': file, 'cloudinary_url': srcURL })
-
   print(srcURL)
 
-# Upload files from folder``
-# ==============================
-# cloudinary.uploader.upload(files[0], folder="TH_assets")
-
-# srcURL = cloudinary.CloudinaryImage(files).build_url()
-
-# print(srcURL)
+with open('videos.csv', 'w') as csvfile:
+  filewriter = csv.writer(csvfile, quotechar='|', quoting=csv.QUOTE_MINIMAL, dialect="excel")
+  filewriter.writerow(['title', 'cloudinary_url'])
+  for file in files:
+    name = Path(file).stem
+    filewriter.writerow([name, srcURL])

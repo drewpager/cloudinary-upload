77105fe702f82b90471490c1fe38ccca57dbe932
drewpager <drewpage@shredlights.com>
2022-12-07 09:57:10 -0800

    updating path

--- get-titles.py
+++ get-titles.py
@@ -5,7 +5,6 @@ from pathlib import Path
 
 # Get File system // UPDATE USING PWD in TERMINAL
 # ==============================
-# mypath = "/Users/drewpage/Desktop/"
 mypath = "/Volumes/Film and Music Backups/Cloudinary-Success"
 files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
